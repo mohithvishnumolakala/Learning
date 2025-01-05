9e24107b79872dc8f9183f8373066146ab426819
mohithvishnumolakala <mohithvishnumolakala@gmail.com>
2025-01-05 10:07:23 +0530

    javascript tasks

---
+++ Demo.js
@@ -0,0 +1,63 @@
+// console.log("print syntax")
+
+// console.log("comment")
+/*multi comments
+zcuisd
+dbcjbjsdck*/
+
+/*var a=10;
+var b="mohith";
+console.log(a,b)
+console.log(a+b); 
+*/
+
+/* operators
+var a=10;
+var b=20;
+console.log(a+b);
+console.log(a**b);
+console.log(a/b);
+console.log(a%b); 
+*/
+
+/* implicit
+let result='3'+2;
+let res='3'+'2';
+let r='4'-true;
+let re=4-true;
+console.log(result);
+console.log(res);
+console.log(r);
+console.log(re);
+*/
+
+/*
+a="mohiTh's lapy" 
+console.log(a);
+console.log(a.length);
+console.log(a.indexOf("h"));
+console.log(a.charAt(3));
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+*/
+
+/* array
+let a=["mohith","geetha","work"]
+console.log(a);
+console.log(a[2]);
+*/
+
+/*
+if(1>2)
+{
+    console.log("true")
+}
+else if(1>3)
+    { console.log("nothing") }
+else
+{
+    console.log("false")
+}
+*/
+
+
